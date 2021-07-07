25bcb42f514ab9ce9f37f9f097936a25cb950de1
Dax Douglass <dax.douglass@gmail.com>
2021-07-07 16:18:51 -0400

    IT213_Douglass_Unit1

--- IT213_Douglass_Unit1/Program.cs
+++ IT213_Douglass_Unit1/Program.cs
@@ -12,11 +12,11 @@ namespace IT213_Douglass_Unit1
             string city = "AnyTown";
             string state = "TX";
             string zip = "11111";
-            string units_taken = "19";
-            int int_units_taken = Int32.Parse(units_taken);      // Convert string to int
-            int inc_units_taken = ++int_units_taken;                // Increment by 1
-            double price_per_unit = 100.50;
-            double tuition = price_per_unit * inc_units_taken; // Calculate tuition
+            string unitsTaken = "19";
+            int intUnitsTaken = Int32.Parse(unitsTaken);      // Convert string to int
+            int incUnitsTaken = ++intUnitsTaken;              // Increment by 1
+            double pricePerUnit = 100.50;
+            double tuition = pricePerUnit * incUnitsTaken;    // Calculate tuition
             int discount = 150;
             double afterDiscount = tuition - discount;        // Calculate tuition after discount
             double monthlyPayment = afterDiscount / 12;       // Calculate monthly payment
@@ -32,7 +32,7 @@ namespace IT213_Douglass_Unit1
             Console.WriteLine(" ");
             Console.WriteLine("Zip Code: " + zip);
             Console.WriteLine(" ");
-            Console.WriteLine("The number of units taken is: " + inc_units_taken);
+            Console.WriteLine("The number of units taken is: " + incUnitsTaken);
             Console.WriteLine(" ");
             Console.WriteLine("The tuition before discount is " + tuition.ToString("C"));
             Console.WriteLine(" ");
